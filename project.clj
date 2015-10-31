e047a6f61d1a07b68d4329b8c6f5e1c941199292
Aliaksei Belablotski <beloblotskiy@gmail.com>
2015-10-30 20:37:07 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject export_import "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The export_import utility"
+  :url "http://belablotski.blogspot.com/2015/10/the-exportimport-utility-or-just-tiny.html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
